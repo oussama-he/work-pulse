923a6b1a8eda0a92340d872a42f604a87ddb0177
oussama <oussama.hel@gmail.com>
2025-04-09 12:20:09 +0100

    change request headers

--- core/scrapers.py
+++ core/scrapers.py
@@ -9,8 +9,15 @@ from django.utils import timezone
 from slugify import slugify
 
 REQUEST_HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-    "(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/108.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,"
+    "*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Referer": "https://www.google.com/",  # Change to a plausible referer for the target site
+    "DNT": "1",
+    "Upgrade-Insecure-Requests": "1",
 }
 
 SOURCES_CONFIG = {
